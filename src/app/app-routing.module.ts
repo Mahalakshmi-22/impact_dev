ec5eb2dc88937e9683e633eeae7937c84387faf9
Shamili Anbuselvan <shamili.a@finstein.ai>
2023-06-20 23:26:55 +0530

    components add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { TeamComponent } from './team/team.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+import { LayoutComponent } from './layout/layout.component';
+const routes: Routes = [
+  {
+    path: 'signup',
+    component: SignUpComponent,
+  },
+  {
+    path: 'team',
+    component: TeamComponent,
+  }, {
+    path: 'dashboard',
+    component: DashboardComponent,
+  }, {
+    path: 'employeeDetails',
+    component: EmployeeDetailsComponent,
+  }, {
+    path: 'layout',
+    component: LayoutComponent,
+  },
+];
 
-const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ButtonModule } from 'primeng/button';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { TeamComponent } from './team/team.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpComponent,
+    TeamComponent,
+    DashboardComponent,
+    EmployeeDetailsComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
