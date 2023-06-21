257e35e1cde000e4caaf3fc97a9eb8e44671ebb2
karthikesanR <karthikesanraja09@gmail.com>
2023-06-21 19:27:51 +0530

    i complted my form task

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,16 @@ import { TeamComponent } from './team/team.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
 import { LayoutComponent } from './layout/layout.component';
+import { SidebarModule } from 'primeng/sidebar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
+import { DropdownModule } from 'primeng/dropdown';
+import { CheckboxModule } from 'primeng/checkbox';
+import { CalendarModule } from 'primeng/calendar';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+
 
 @NgModule({
   declarations: [
@@ -22,7 +32,16 @@ import { LayoutComponent } from './layout/layout.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ButtonModule
+    ButtonModule,
+    SidebarModule,
+    BrowserAnimationsModule,
+    InputTextModule,
+    FormsModule,
+    DropdownModule,
+    CheckboxModule,
+    CalendarModule,
+    RadioButtonModule,
+    InputTextareaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/employee-details/employee-details.component.ts
+++ src/app/employee-details/employee-details.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component,OnInit} from '@angular/core';
+
+interface City {
+  name: string;
+  code: string;
+}
+
 
 @Component({
   selector: 'app-employee-details',
   templateUrl: './employee-details.component.html',
   styleUrls: ['./employee-details.component.scss']
 })
-export class EmployeeDetailsComponent {
 
+export class EmployeeDetailsComponent implements OnInit {
+  
+  value:any
+  cities: City[] | undefined;
+
+  selectedCity: City | undefined;
+  selectedCities: string[] = [];
+  date: any;
+  ingredient!: string;
+
+  disabled: boolean = false;
+
+  
+  constructor() { }
+  ngOnInit(): void {
+    this.cities = [
+      { name: 'New York', code: 'NY' },
+      { name: 'Rome', code: 'RM' },
+      { name: 'London', code: 'LDN' },
+      { name: 'Istanbul', code: 'IST' },
+      { name: 'Paris', code: 'PRS' }
+  ];
 }
+sidebarVisible: boolean = false;
+
+  }
+ 
+
+
